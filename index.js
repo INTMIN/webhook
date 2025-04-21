2402f6207000ef9f3af00de8dc3adabdefb137c4
MIN <min_word@qq.com>
2025-04-21 17:33:44 +0800

    feat: 测试提交

--- index.js
+++
@@ -1,5 +0,0 @@
-const createHandler = require("azure-function-express").createHandler;
-const expressApp = require("./app");
-
-// 使用 Azure Function Express Bridge
-module.exports = createHandler(expressApp);
